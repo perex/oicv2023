9ad5107f8dba7f4c7ee324f0dd17717f97c1428e
Jose Manuel Pérez <jmperez27@gmail.com>
2022-10-21 14:19:31 +0200

    Subida del fichero Saludo.py

---
+++ Saludo.py
@@ -0,0 +1,26 @@
+#Ejercicio de preparación - Hola OICV
+#Este es un ejercicio muy sencillo, para que pruebes el funcionamiento del juez de la olimpiada.
+#Debes escribir un programa que lea de la entrada estándar y que escriba varios saludos.
+#La primera línea de la entrada que debes leer tiene dos números n y k. El primer número indica la cantidad de saludos y el segundo indica si hay que leer o no nombres para saludar.
+#En el caso en que sea 0, significa que no hay que leer nada más de la entrada y que hay que saludar n veces diciendo Hola OICV.
+
+#ejemplo 4 0
+linea = input()
+#El primer número indica la cantidad de saludos
+n = int(linea[0]) 
+#el segundo indica si hay que leer o no nombres para saludar
+k = int(linea[2])
+#print(n)
+#print(k)
+if k == 0: #no hay que leer nombres
+    #imprimimos tantos Hola OICV como contenga n
+    for x in range(n):
+        print("Hola OICV")
+elif k == 1:
+    #leer nombres por línea estándar
+    lista = []
+    for x in range(n):
+        nombre = input()
+        lista.append(nombre)
+    for x in lista:
+        print("Hola "+x)
