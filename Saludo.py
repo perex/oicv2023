27c297398f1c53a12f41c46333c75641bf38262b
Jose Manuel Pérez <jmperez27@gmail.com>
2022-11-11 00:15:19 +0100

    Último cambio sobre el ejemplo Saludo
    Domingo Gallardo me indica como funciona el juez y adapto la solución a las recomendaciones del mismo

--- Saludo.py
+++ Saludo.py
@@ -4,35 +4,18 @@
 #La primera línea de la entrada que debes leer tiene dos números n y k. El primer número indica la cantidad de saludos y el segundo indica si hay que leer o no nombres para saludar.
 #En el caso en que sea 0, significa que no hay que leer nada más de la entrada y que hay que saludar n veces diciendo Hola OICV.
 
-#ejemplo 4 0
-linea = input()
-
-try:   
+#El primer número indica la cantidad de saludos
+n = int(linea[0]) 
+#el segundo indica si hay que leer o no nombres para saludar
+k = int(linea[2])
     
-    #El primer número indica la cantidad de saludos
-    n = int(linea[0]) 
-    #el segundo indica si hay que leer o no nombres para saludar
-    k = int(linea[2])
-    
-    #print(n)
-    #print(k)
-
+while (n > 0):
     if k == 0: #no hay que leer nombres
-        #imprimimos tantos Hola OICV como contenga n
-        for x in range(n):
-            print("Hola OICV")
+        salida = "Hola OICV"
     elif k == 1:
-        #leer nombres por línea estándar
-        listaNombres = []
-        for x in range(n):
-            nombre = input()
-            listaNombres.append(nombre)
-        for x in listaNombres:
-            #Imprimir "Hola " + x ("Nombre Actual de la lista")
-            print("Hola "+x)
-    else:
-        print("Entrada incorrecta")
-except:
-        print("Entrada incorrecta")   
+        nombre = input()
+        salida = "Hola " + nombre
+    print(salida)
+    n -= 1
     
    
